1ba6d7e9a7b4568ae3af555f8e5fb2835647d3fa
junruanderson <32849447+junruanderson@users.noreply.github.com>
2018-01-29 19:21:48 -0500

    redid all

--- script.js
+++ script.js
@@ -1,70 +1,26 @@
-var allEntranceRelatedClasses = "bounceIn fadeIn flipInY lightSpeedIn rotateInDownLeft slideInRight zoomIn rollIn";
-
-function selectEntrance()
-{
-    var BOUNCE_IN = 0;
-    var FADE_IN = 1;
-    var FLIP_IN_Y = 2;
-    var LIGHT_SPEED_IN = 3;
-    var ROTATE_IN_DOWN_LEFT = 4;
-    var SLIDE_IN_RIGHT = 5;
-    var ZOOM_IN = 6;
-    var ROLL_IN = 7;
-    
-    var numberOfChoices = 8;
-    
-    var exit = Math.floor(Math.random()*numberOfChoices);
-    
-    if(exit==BOUNCE_IN)
-    {
-        return "bounceIn"
-    }
-    else if(exit == FADE_IN)
-    {
-        return "fadeIn";
-    }
-    else if(exit == FLIP_IN_Y)
-    {
-        return "flipInY";
-    }
-    else if(exit == LIGHT_SPEED_IN)
-    {
-        return "lightSpeedIn";
-    }
-    else if(exit == ROTATE_IN_DOWN_LEFT)
-    {
-        return "rotateInDownLeft";
-    }
-    else if (exit == SLIDE_IN_RIGHT)
-    {
-        return "slideInRight";
-    }
-    else if(exit == ZOOM_IN)
-    {
-        return "zoomIn";
-    }
-    else if(exit == ROLL_IN)
-    {
-        return "rollIn";
-    }
-    else
-    {
-        return "bounceIn";
-    }
+function show(sectionToShow){
+	$("#" + sectionToShow).show()
+    $("#" + sectionToShow).addClass("slideInRight");
 }
 
-function show(sectionToShow)
-{
-    $(".section").removeClass(allEntranceRelatedClasses);
-    $(".section").addClass("hidden");
-    $("#" + sectionToShow).removeClass("hidden");
-    $("#" + sectionToShow).addClass(selectEntrance());
+function hide(sectionToHide){
+    $("#" + sectionToHide).addClass("slideOutRight");
+	window.setTimeout(function(){
+		$("#" + sectionToHide).hide();
+		$("#" + sectionToHide).removeClass("slideOutRight slideInRight");
+	}, 1000);
+	
 }
 
-function removeDefault()
+function show_slider(){
+	$(".slider").show();
+	window.setTimeout(function(){
+		$(".slider").hide();
+	}, 1300)
+}
+
+
+function test()
 {
-    $("#default").addClass("slideOutLeft");
-    $("#default").addClass("hidden");
-    $("#profile").removeClass("hidden");
-    $("#profile").addClass(selectEntrance());
+	alert("hello");
 }
